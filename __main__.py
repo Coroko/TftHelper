66ffd1d045c2b6be8307936d658002f15744e921
Coroko <45878223+Coroko@users.noreply.github.com>
2022-01-14 16:49:32 +0100

    comments

--- __main__.py
+++ __main__.py
@@ -6,32 +6,34 @@ def populate_db():
     sql.start_db()
     conexion=sql.openConnection()
     cur= conexion.cursor()
-    user= ["Corokoo","harry32xd","CazadorDeMinitas","infernixx","ReventXz v2"]
+    user= ["Corokoo","harry32xd","CazadorDeMinitas","infernixx","ReventXz v2"] #jugadores analizar
     for name in user :
         data=api.getUser(name)
         sql.insertIntoTablePlayers(cur,data)
-    cur.execute('''select puuid from players''')
+    cur.execute('''select puuid from players''') #Una vez añadidos para no hacer tantas llamadas a la api, cojemos los puuid del sqlite
     for row in cur.fetchall():
         puuidofplayer = ''.join(row)
         data=api.getAllMatches(puuidofplayer)
-        for matchid in data :
+        for matchid in data : #Obtenidas todas las partidas, procederemos a sacar todos los puntos importantes de ellas
             game_hash=sql.createGameHash(puuidofplayer,matchid)
             gamedata=api.getMatchById(matchid)
-            for participant in gamedata["info"]["participants"]:
+            for participant in gamedata["info"]["participants"]: 
                 sql.insertIntoTableGames(cur,participant,game_hash,matchid)
                 puuid=participant["puuid"]
                 for units in participant["units"]:
                     sql.insertIntoTableUnits(cur,units,game_hash,puuid)
                 for traits in participant["traits"]:
                     sql.insertIntoTableTraits(cur,traits,game_hash,participant["puuid"])
-                
+                #tras haber hecho esto obtrendemos una base de datos bastante consolidada, con un total entre todas
+                #sus filas al rededor de los 3000 datos, y esto solo con 5 jugadores y 4 partidas con cada uno.
     sql.closeConnection(cur)
     conexion.commit()     
 
 def start_cnn():
-        
+    print("hello world")        
 def main():
-    populate_db()
+    #populate_db()
+    start_cnn()
 
 if __name__=='__main__':
     main()
